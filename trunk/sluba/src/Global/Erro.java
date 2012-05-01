d100d994b278fbe824c13f59cb4cd5430688ad00
luizgustavodesouza@gmail.com <luizgustavodesouza@gmail.com@c6214885-86aa-9432-6443-85473d33e6e3>
2012-05-01 18:35:40 +0000

    oi

--- trunk/sluba/src/Global/Erro.java
+++ trunk/sluba/src/Global/Erro.java
@@ -13,12 +13,18 @@ public class Erro {
     public static void throwError(int error) {
 
         switch (error) {
-            //Erro 01: Arquivo de entrada nao encontrado.
+            //Erro 01: Arquivo de entrada nao especificado.
             case 1:
-                System.out.println("sluba: erro fatal: nao ha arquivos de entrada");
-                System.out.println("Utilize o parametro -h para ajuda");
-                System.out.println("Compilacao interrompida");
+                System.out.println("sluba: Erro 01: nao ha arquivos de entrada");
+                break;
+            //Erro 02: Arquivo de entrada nao especificado.
+            case 2:
+                System.out.println("sluba: Erro 02: Arquivo ou pasta nao existe");
+                break;
         }
+
+        System.out.println("Utilize o parametro -h para ajuda");
+        System.out.println("Compilacao interrompida.");
         System.exit(1);
     }
 }
--- trunk/sluba/src/sluba/Sluba.java
+++ trunk/sluba/src/sluba/Sluba.java
@@ -19,13 +19,17 @@ public class Sluba {
      */
     public static void main(String[] args) {
 
-        
+        boolean verbose = false;
+        boolean output = false;
+        String oPath = null;
+
+        //Verficia se ha arquivo de entrada
         if (args.length == 1) {
             Erro.throwError(1);
         }
-        
+
         //help
-        if(args[1].compareTo("-h")==0) {
+        if (args[1].compareTo("-h") == 0) {
             System.out.println("Uso: sluba <opcao> <arquivo> <parametro>");
             System.out.println("<opcao> e <parametro> NAO sao obrigatorios\n\n");
             System.out.println("opcao:");
@@ -34,22 +38,51 @@ public class Sluba {
             System.out.println("arquivo:\tarquivo ou diretorio para arquivo a ser compilado");
             System.out.println("\n");
             System.out.println("parametro:\n");
-            System.out.println("\t-o arquivo\tCria o arquivo compilado em diretorio e nome especificos, incluindo extensao");
+            System.out.println("\t-o arquivo\tOutput: Cria o arquivo compilado em diretorio e nome especificos, incluindo extensao");
+            System.out.println("\t-v \t\tVerbose: Expressa em texto os processos do compilador");
             System.out.println("\n");
             System.out.println("Spam, Louis and Drumstick corp.");
+            System.exit(0);
         }
-        
+
+
+        //Abre o arquivo
         File file;
-        
-        if(args[1].substring(0, 1).compareTo("/")==0) {
+        if (args[1].substring(0, 1).compareTo("/") == 0) {
             file = new File(args[1]);
         } else {
-            file = new File(args[0]+"/"+args[1]);
+            file = new File(args[0] + "/" + args[1]);
         }
+         
+        //Verifica existencia do arquivo
+        if (!file.exists()) {
+            Erro.throwError(2);
+        }
+
+        System.out.println(file.getName().split("."));
+        System.out.println(file.getName());
         
+        //Analisa os parametros de entrada
         for (int i = 2; i < args.length; i++) {
-            //analisa os parametros
+            if (args[i].equals("-o")) {
+                output = true;
+                oPath = args[i + 1];
+            } else {
+                if (args[i].equals("-v")) {
+                    verbose = true;
+                }
+            }
+        }
+
+        //Verbose
+        if(verbose) {
+            System.out.println("Arquivo de entrada aceito com sucesso para:");
+            System.out.println(file.getAbsolutePath());
+            System.out.println();
         }
         
+        //Inicio de Modulo Lexico
+        System.out.println("Iniciando analise lexica:\n\n");
+
     }
 }
