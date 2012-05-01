9d53b3a1753b75b674e4e18a671a4d48b2210346
luizgustavodesouza@gmail.com <luizgustavodesouza@gmail.com@c6214885-86aa-9432-6443-85473d33e6e3>
2012-05-01 17:45:27 +0000

    oi

--- sluba/src/Global/PalavraReservada.java
+++ sluba/src/Global/PalavraReservada.java
@@ -9,5 +9,53 @@ package Global;
  * @author luiz-mint
  */
 public class PalavraReservada {
+    Hash tabela;
+    
+    public PalavraReservada() {
+        //Adiciona todas as palavras reservadas na tabela
+        tabela = new Hash();
+        tabela.addSimbolo("int");
+        tabela.addSimbolo("float");
+        tabela.addSimbolo("char");
+        tabela.addSimbolo("[");
+        tabela.addSimbolo("]");
+        tabela.addSimbolo("(");
+        tabela.addSimbolo(")");
+        tabela.addSimbolo("{");
+        tabela.addSimbolo("}");
+        tabela.addSimbolo(";");
+        tabela.addSimbolo("//");
+        tabela.addSimbolo("+");
+        tabela.addSimbolo("-");
+        tabela.addSimbolo("*");
+        tabela.addSimbolo("/");
+        tabela.addSimbolo("%");
+        tabela.addSimbolo("!=");
+        tabela.addSimbolo("==");
+        tabela.addSimbolo(">");
+        tabela.addSimbolo("<");
+        tabela.addSimbolo(">=");
+        tabela.addSimbolo("<=");
+        tabela.addSimbolo("||");
+        tabela.addSimbolo("&&");
+        tabela.addSimbolo("read");
+        tabela.addSimbolo("write");
+        tabela.addSimbolo("procedure");
+        tabela.addSimbolo("if");
+        tabela.addSimbolo("for");
+        tabela.addSimbolo("inc");
+        tabela.addSimbolo("dec");
+        tabela.addSimbolo("break");
+        tabela.addSimbolo("Start");
+        tabela.addSimbolo("Finish.");
+    }
+
+    public Hash getTabela() {
+        return tabela;
+    }
+
+    public void setTabela(Hash tabela) {
+        this.tabela = tabela;
+    }
     
 }
