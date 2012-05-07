536faa575a7987b2e2c069fca388d36fad17ab5d
luizgustavodesouza@gmail.com <luizgustavodesouza@gmail.com@c6214885-86aa-9432-6443-85473d33e6e3>
2012-05-07 17:04:57 +0000

    Lexico

--- sluba/src/Global/Erro.java
+++ sluba/src/Global/Erro.java
@@ -4,27 +4,59 @@
  */
 package Global;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author luiz-mint
  */
 public class Erro {
 
-    public static void throwError(int error) {
+    ArrayList<String> erros;
+
+    public Erro() {
+        erros = new ArrayList();
+    }
+
+    public void throwError(int error, String component) {
 
         switch (error) {
             //Erro 01: Arquivo de entrada nao especificado.
             case 1:
-                System.out.println("sluba: Erro 01: nao ha arquivos de entrada");
+                erros.add("sluba: Erro Fatal: nao ha arquivos de entrada");
+                printError();
+                System.out.println("Utilize o parametro -h para ajuda");
+                System.out.println("Compilacao interrompida.");
+                System.exit(1);
                 break;
             //Erro 02: Arquivo de entrada nao especificado.
             case 2:
-                System.out.println("sluba: Erro 02: Arquivo ou pasta nao existe");
+                erros.add("sluba: Erro Fatal: Arquivo ou pasta nao existe");
+                printError();
+                System.out.println("Utilize o parametro -h para ajuda");
+                System.out.println("Compilacao interrompida.");
+                System.exit(1);
                 break;
+                
+            case 3:
+                //Erro 03: Simbo lido pelo Lexico e invalido
+                erros.add("Erro linha " + component + ": Simbolo nao reconhecido.");
+                break;
+            case 4:
+                erros.add("Erro linha " + component + ": Uso incorreto do simbolo '\"'.");
+                break;
+                
         }
 
-        System.out.println("Utilize o parametro -h para ajuda");
-        System.out.println("Compilacao interrompida.");
-        System.exit(1);
+        /*
+         * System.out.println("Utilize o parametro -h para ajuda");
+         * System.out.println("Compilacao interrompida."); System.exit(1);
+         */
+    }
+
+    public void printError() {
+        for (String string : erros) {
+            System.out.println(string);
+        }
     }
 }
--- sluba/src/sluba/Sluba.java
+++ sluba/src/sluba/Sluba.java
@@ -5,8 +5,13 @@
 package sluba;
 
 import Global.Erro;
-import Global.PalavraReservada;
+import Global.TabelaDeSimbolos;
+import Lexico.Lexico;
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,13 +24,23 @@ public class Sluba {
      */
     public static void main(String[] args) {
 
+        Erro erro = new Erro();
         boolean verbose = false;
         boolean output = false;
         String oPath = null;
 
+        //Tabela de Simbolos
+        TabelaDeSimbolos tb = new TabelaDeSimbolos();
+        //Modulos
+        Lexico lexico = new Lexico(tb, verbose, erro);
+
+        //Arquivo
+        File file;
+        int f = 1;
+
         //Verficia se ha arquivo de entrada
         if (args.length == 1) {
-            Erro.throwError(1);
+            erro.throwError(1, null);
         }
 
         //help
@@ -34,55 +49,59 @@ public class Sluba {
             System.out.println("<opcao> e <parametro> NAO sao obrigatorios\n\n");
             System.out.println("opcao:");
             System.out.println("\t-h\tMostra estas informacoes");
+            System.out.println("\t-v \tVerbose: Expressa em texto os processos do compilador");
             System.out.println("\n");
             System.out.println("arquivo:\tarquivo ou diretorio para arquivo a ser compilado");
             System.out.println("\n");
             System.out.println("parametro:\n");
-            System.out.println("\t-o arquivo\tOutput: Cria o arquivo compilado em diretorio e nome especificos, incluindo extensao");
-            System.out.println("\t-v \t\tVerbose: Expressa em texto os processos do compilador");
+            System.out.println("\t-o <arquivo>\tOutput: Cria o arquivo compilado em diretorio e nome especificos, incluindo extensao");
             System.out.println("\n");
             System.out.println("Spam, Louis and Drumstick corp.");
             System.exit(0);
         }
 
 
+        //Verbose
+        if (args[1].equals("-v")) {
+            verbose = true;
+            System.out.println("Opcao Verbose ativada.");
+            f++;
+        }
+
         //Abre o arquivo
-        File file;
-        if (args[1].substring(0, 1).compareTo("/") == 0) {
-            file = new File(args[1]);
+        if (args[f].substring(0, 1).compareTo("/") == 0) {
+            file = new File(args[f]);
         } else {
             file = new File(args[0] + "/" + args[1]);
         }
-         
+
         //Verifica existencia do arquivo
         if (!file.exists()) {
-            Erro.throwError(2);
+            erro.throwError(2, null);
         }
 
-        System.out.println(file.getName().split("."));
-        System.out.println(file.getName());
-        
         //Analisa os parametros de entrada
         for (int i = 2; i < args.length; i++) {
             if (args[i].equals("-o")) {
                 output = true;
                 oPath = args[i + 1];
-            } else {
-                if (args[i].equals("-v")) {
-                    verbose = true;
-                }
             }
         }
 
         //Verbose
-        if(verbose) {
+        if (verbose) {
             System.out.println("Arquivo de entrada aceito com sucesso para:");
             System.out.println(file.getAbsolutePath());
             System.out.println();
         }
-        
+
         //Inicio de Modulo Lexico
         System.out.println("Iniciando analise lexica:\n\n");
+        try {
+            lexico.readFile(new RandomAccessFile(file, "r"));
+        } catch (IOException ex) {
+            Logger.getLogger(Sluba.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
